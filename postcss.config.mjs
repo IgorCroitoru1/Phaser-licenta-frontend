c25fb80e1776bf0db8e6f5b8749eb076fdf10c1e
IgorCroitoru1 <igor.croitoru@student.usv.ro>
2025-05-10 02:09:31 +0300

    postcss fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 const config = {
     plugins: {
       "@tailwindcss/postcss": {},
-      tailwindcss: {},
-      autoprefixer: {},
+      // tailwindcss: {},
+      // autoprefixer: {},
     },
   };
   export default config;
